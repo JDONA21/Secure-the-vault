0c437220bd1fd69a32d215beb73e31028cfc9018
Jesus Cardona Jr <jesus.a.cardona21@gmail.com>
2026-09-14 12:21:00 -0600

    completed secure the vault

---
+++ index.js
@@ -0,0 +1,10 @@
+const code1;
+code1 = 7 + 3;
+const code2 = 60 - 40 / 2;
+const code3 = 40 - (7 % 3);
+const message = "The vault has been secured. The combination is:";
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message);
+console.log(codeA);
+console.log(codeB);
